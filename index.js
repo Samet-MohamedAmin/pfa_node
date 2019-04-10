1f9d78cd5c6e79cec77af9a27fd742f7487fae1c
mohamedamin <mohamedamin.samet@gmail.com>
2019-04-10 23:58:16 +0100

    add production environement and fix mongoose deprication warning

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser  = require ('body-parser')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
 const passport = require('passport');
@@ -7,21 +7,39 @@ var cors = require('cors')
 
 const http = require('http')
 
+let mongo_url;
+/* Setting up developement environment */
+const NODE_ENV = process.env.NODE_ENV;
+if (NODE_ENV == 'production') {
+  const MONGO_USER = process.env.MONGO_USER;
+  const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+  mongo_url = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@4c-vhflf.gcp.mongodb.net/test?retryWrites=true&&authSource=admin`;
+}
+else { mongo_url = 'mongodb://localhost:27017/4c' }
 
-const connexion = mongoose.connect('mongodb://localhost:27017/4c',function(err){
-    if(err){
-      console.log("Problem when connecting to the database")
-    }})
+/* fix mongoose deprication warnings */
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+const connexion = mongoose.connect(mongo_url, function (err) {
+  if (err) {
+    console.log("Problem when connecting to the database")
+  }
+  else {
+    console.log('SUCCESSFULLY connected to mongo')
+  }
+})
 const app = express()
 
-app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     res.status(401);
-    res.json({"message" : err.name + ": " + err.message});
+    res.json({ "message": err.name + ": " + err.message });
   }
   res.header('Access-Control-Allow-Origin', req.headers.origin) // <-- you can change this with a specific url like http://localhost:4200
   res.header('Access-Control-Allow-Credentials', true)
